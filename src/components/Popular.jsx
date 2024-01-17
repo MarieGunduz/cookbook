1850c9bb8e20e70a4824fab97696ccf455a520af
Marie Gunduz <mariegunduz@gmail.com>
2024-01-17 10:40:11 +0300

    add: fetching recipes in popular component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 function Popular() {
+  const varA = 3;
+  useEffect(() => {
+    getPopular()
+  }, [varA])
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&exclude-tags=dairy`)
+    const data = await api.json();
+    console.log(data);
+  }
+
   return (
     <div>Popular</div>
   )
