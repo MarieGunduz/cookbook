fddf11f9c5819dac1e2a0266b3c7e8990cef9ce9
Marie Gunduz <mariegunduz@gmail.com>
2024-01-17 09:30:19 +0300

    add:two new components Popular and Veggies

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Veggies from "../components/Veggies";
+import Popular from "../components/Popular";
+
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Veggies />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
