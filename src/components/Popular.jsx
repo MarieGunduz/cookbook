a9a533dce4bd055ad0791d31e6e2fa0cf0e98e5d
Marie Gunduz <mariegunduz@gmail.com>
2024-01-24 11:34:11 +0300

    add: new libraries, recipe mapping

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 
 function Popular() {
-  const [popular, setPopular] = useState();
-  // console.log("POP", popular)
+  const [popular, setPopular] = useState([]);
   useEffect(() => {
     getPopular()
-  }, [popular])
+  }, [])
 
   const getPopular = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9&exclude-tags=dairy`)
     const data = await api.json();
     setPopular(data.recipes)
-   // console.log(data);
-  }
+  };
 
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe?.id}>
+            <p>{recipe?.title}</p>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
