6eb5eb692208f54273142245cf35e091b0777528
Marie Gunduz <mariegunduz@gmail.com>
2024-02-06 14:12:56 +0300

    changed codes in popular and worked on cuisine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   const key = process.env.REACT_APP_API_KEY;
   console.log(key)
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Pages />      
+      <BrowserRouter>
+      <Category />
+      <Pages />
+      </BrowserRouter>      
     </div>
   );
 }
