cad44633416622404b79d3dd4c93ae50aab5d026
Marie Gunduz <mariegunduz@gmail.com>
2024-02-15 12:50:03 +0300

    updated cuisine, created search, created searched

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from 'react-router-dom';
 import Cuisine from "./pages/Cuisine";
+import Search from "./components/Search";
 
 function App() {
   const key = process.env.REACT_APP_API_KEY;
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
         <Cuisine />
