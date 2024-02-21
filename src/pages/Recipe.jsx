ea4fa10703673914498dec4afb7254e198d26ede
Marie Gunduz <mariegunduz@gmail.com>
2024-02-21 21:33:13 +0300

    adding buttons, instructions, ingredients

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -8,12 +8,14 @@ function Recipe() {
 
     let params = useParams();
     const [details, setDetails] = useState({});
+    const [activeTab, setActiveTab] = useState('instructions');
 
     const fetchDetails = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
         );
         const detailData = await data.json();
         setDetails(detailData);
+        console.log(detailData);
     };
 
     useEffect(() => {
@@ -27,9 +29,17 @@ function Recipe() {
         <img src={details.image} alt="" />
     </div>
     <Info>
-        <Button>Instructions</Button>
-        <Button>Ingredients</Button>
-    </Info>
+        <Button className={activeTab === 'instructions' ? 'active' : ''} onClick={() => setActiveTab("instructions")}>
+            Instructions
+         </Button>
+        <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab("ingredients")}>
+            Ingredients
+        </Button>
+        <div>
+            <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+            <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+        </div>
+       </Info>
     </DetailWrapper>
 };
 
